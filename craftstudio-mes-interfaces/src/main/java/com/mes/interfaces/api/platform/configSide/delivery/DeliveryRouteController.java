b83cb6faee1ad9ee0b2e8a5084a9c9e4611b6750
达达 <11375489+dada9504@user.noreply.gitee.com>
2026-06-11 15:27:08 +0800

    1

--- craftstudio-mes-interfaces/src/main/java/com/mes/interfaces/api/platform/configSide/delivery/DeliveryRouteController.java
+++ craftstudio-mes-interfaces/src/main/java/com/mes/interfaces/api/platform/configSide/delivery/DeliveryRouteController.java
@@ -192,7 +192,7 @@ public class DeliveryRouteController {
             @Valid @RequestBody AddressRecognitionRecordListRequest request) {
         PagedQuery query = request.toPagedQuery();
         PagedResult<AddressRecognitionRecordResponse> result = appDeliveryRouteService.listUnassignedAddressRecognitionRecords(query);
-        return PagedApiResponse.success(result.items(), query.getCurrent(), query.getSize(), result.total());
+        return PagedApiResponse.success((List<AddressRecognitionRecordResponse>) result.items(), query.getCurrent(), query.getSize(), result.total());
     }
 
     @PostMapping("/address-recognition/bind")
