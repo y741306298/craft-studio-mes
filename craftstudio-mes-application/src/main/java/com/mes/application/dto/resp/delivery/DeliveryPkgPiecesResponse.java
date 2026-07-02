b10aee8bc2a1120d4be263181cc5693b4a185c58
y741306298 <69667134+y741306298@users.noreply.github.com>
2026-07-02 21:06:57 +0800

    Polish delivery package API docs examples

--- craftstudio-mes-application/src/main/java/com/mes/application/dto/resp/delivery/DeliveryPkgPiecesResponse.java
+++ craftstudio-mes-application/src/main/java/com/mes/application/dto/resp/delivery/DeliveryPkgPiecesResponse.java
@@ -4,6 +4,7 @@ import com.mes.application.command.delivery.vo.DeliveryPkgPieceVO;
 import com.mes.domain.delivery.deliveryRoute.vo.OrgInfo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
@@ -13,7 +14,15 @@ public class DeliveryPkgPiecesResponse {
     private List<DeliveryPkgPieceVO> items;
     private List<String> materialList;
     private List<Double> sizeList;
-    private List<String> processList;
+    private List<ProcessingFlowOption> processList;
     private OrgInfo orgInfo;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ProcessingFlowOption {
+        private String processName;
+        private String accessoryName;
+    }
 }
 
