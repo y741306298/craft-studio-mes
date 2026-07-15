1ce763cb9274b2a463e5293b88a8eadc4e1512cd
y741306298 <69667134+y741306298@users.noreply.github.com>
2026-07-15 21:36:05 +0800

    fix: support ObjectId lookup for typesetting callbacks

--- craftstudio-mes-infra/src/main/java/com/mes/infra/dal/manufacurer/typesetting/TypesettingRepositoryImp.java
+++ craftstudio-mes-infra/src/main/java/com/mes/infra/dal/manufacurer/typesetting/TypesettingRepositoryImp.java
@@ -4,6 +4,10 @@ import com.mes.domain.manufacturer.typesetting.entity.TypesettingInfo;
 import com.mes.domain.manufacturer.typesetting.repository.TypesettingRepository;
 import com.mes.infra.base.BaseRepositoryImp;
 import com.mes.infra.dal.manufacurer.typesetting.po.TypesettingPo;
+import com.mes.infra.db.mongodb.SoftDeleteQuery;
+import org.apache.commons.lang3.StringUtils;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -12,5 +16,26 @@ public class TypesettingRepositoryImp extends BaseRepositoryImp<TypesettingInfo,
     @Override
     public Class<TypesettingPo> poClass() {
         return TypesettingPo.class;
+    }
+
+    @Override
+    public TypesettingInfo findById(String id) {
+        TypesettingPo po = findPoById(id);
+        return po == null ? null : po.toDO();
+    }
+
+    private TypesettingPo findPoById(String id) {
+        if (StringUtils.isBlank(id)) {
+            return null;
+        }
+        TypesettingPo po = mongoTemplate.findOne(
+                new SoftDeleteQuery(Criteria.where("_id").is(id)), poClass()
+        );
+        if (po != null || !ObjectId.isValid(id)) {
+            return po;
+        }
+        return mongoTemplate.findOne(
+                new SoftDeleteQuery(Criteria.where("_id").is(new ObjectId(id))), poClass()
+        );
     }
 }
